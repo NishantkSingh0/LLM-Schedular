eeafa3b501307da80d4d3fa92b880f873e9fcd70
NishantkSingh0 <nishant18x.org@gmail.com>
2025-09-14 20:13:21 +0530

    Resolved Router and Path navigation problem

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { Routes, Router , HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 )
