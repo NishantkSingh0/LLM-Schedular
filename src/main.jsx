395b1d74544c00e01943dcfd076b00318d2ee38f
NishantkSingh0 <nishant18x.org@gmail.com>
2025-09-23 18:15:39 +0530

    Resolved Index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { HashRouter } from 'react-router-dom';
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './style.css'
+import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
